038fa329edd85db8aacff7f42cf9544ef7692994
Md Habibur Rahman <typetohabib@gmail.com>
2023-03-14 17:46:32 +0600

    react project started

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./views/header";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./styles/style.css";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header></Header>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+    </React.Fragment>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/views/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">Logos</div>
+
+        <input />
+      </header>
+    );
+  }
+}
+
+export default Header;
