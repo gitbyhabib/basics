8e166eacbcdfa6077ab426405e8478ebb1a1b16e
Md Habibur Rahman <typetohabib@gmail.com>
2023-03-15 13:30:27 +0600

    state declearation done

--- src/views/header.js
+++ src/views/header.js
@@ -1,19 +1,37 @@
 import React from "react";
 
 class Header extends React.Component {
+  state = {
+    name: "habibi",
+    title: "keywords are",
+    keywords: "",
+    count: 0,
+  };
 
-inputChangeHandle(event){
-  console.log(event.target.value)
-}
+  inputChangeHandle = (event) => {
+    this.setState({
+      keywords: event.target.value,
+    });
+  };
 
-
-
+  addone() {
+    this.setState((state, props) => ({
+      count: state.count + 1,
+    }));
+  }
   render() {
+    console.log(this.state);
     return (
       <header>
         <div className="logo">Logos</div>
 
         <input onChange={this.inputChangeHandle} />
+
+        <div>{this.state.title}</div>
+        <div>{this.state.keywords}</div>
+        <div> Count is:{this.state.count}</div>
+
+        <button onClick={() => this.addone()}>Add One</button>
       </header>
     );
   }
